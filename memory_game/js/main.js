17d6ea8898f1ed8e20846e3614065609b4647161
Tereza K <terezak@Tmac.local>
2018-09-03 18:13:08 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,6 +20,8 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png", 
 	}
 ];
+
+
 var cardsInPlay = []; 
 
 var checkForMatch = function () {
@@ -30,19 +32,30 @@ var checkForMatch = function () {
 		}
 }
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank); 
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2)
-		checkForMatch();
-
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	console.log("User flipped " + cards[cardId].rank); 
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard); 
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
 
 
 
